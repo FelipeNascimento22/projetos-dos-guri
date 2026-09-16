9fa87a3e306ebef1cf33b500a0ffb0d4088f4e29
Felipe <felipe_nascimento161@estudante.sesisenai.org.br>
2026-09-16 16:09:45 -0300

    Terminei completamente a minha parte do trabalho.

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('cadastro.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
     nome = request.form['nome']
     email = request.form['email']
     senha = request.form['senha']
+    if not nome or not email or not senha:
+        return "Todos os campos são obrigatórios!"
+    
     print(f"Usuário cadastrado com sucesso! Nome: {nome}, Email: {email}, Senha: {senha}")
-    return render_template('index.html', nome=nome, email=email, senha=senha)
+    return render_template('cadastro.html', nome=nome, email=email, senha=senha)
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
 if __name__ == '__main__':
     app.run(debug=True) 
